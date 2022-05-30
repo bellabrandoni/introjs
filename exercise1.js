b22e1fc3cb48aaef6c7987ea83d010935b68789b
unknown <isabella14brandao@gmail.com>
2022-05-30 14:31:10 -0300

    exercicios coletivos e exercicio pratico

---
+++ exercise1.js
@@ -0,0 +1,11 @@
+const nome = 'Isabella';
+const idade = 22;
+const altura = 1.70;
+let verdadeiro = true;
+let falso = false;
+
+console.log(
+    'Meu nome é ${nome}, eu tenho ${idade}, minha altura é ${altura}'
+);
+
+console.log(`Amo tecnologia: ${verdadeiro}`)
---
+++ intro.js
@@ -0,0 +1,14 @@
+const  nomePais = 'Brasil';
+console.log(nomePais);
+
+let pontoTuristicos = 'Cristo Redentor';
+console.log(pontoTuristicos);
+
+const capital = 'Brasilia';
+console.log(capital);
+
+const numeroEstados = 27;
+console.log(numeroEstados);
+
+const americaLatina = 'Sim';
+console.log(americaLatina);
